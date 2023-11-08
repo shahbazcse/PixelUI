7268fbf3229b0ba637204873baebf0acbddc7cb3
shahbazcse <cseshahbaz@gmail.com>
2023-11-09 01:38:09 +0530

    Designed Button Components

--- pixelui/src/pages/documentation/components/Button/Button.jsx
+++ pixelui/src/pages/documentation/components/Button/Button.jsx
@@ -4,8 +4,14 @@ import "./Button.css";
 import { HiArrowSmallRight } from "react-icons/hi2";
 import { BsBoxArrowUpRight } from "react-icons/bs";
 import { BiSolidCloudUpload } from "react-icons/bi";
-import { AiFillDelete } from "react-icons/ai";
+import { AiOutlineDelete } from "react-icons/ai";
 import { IoAddOutline } from "react-icons/io5";
+import { BsFillCameraFill } from "react-icons/bs";
+import { AiOutlineHeart } from "react-icons/ai";
+import { BsBookmark } from "react-icons/bs";
+import { LiaEditSolid } from "react-icons/lia";
+import { FiSearch } from "react-icons/fi";
+import CodeBlock from "../../../../components/CodeBlock/CodeBlock";
 
 const ButtonComponent = ({ type, color, radius, children, btnText }) => {
   switch (type) {
@@ -30,9 +36,17 @@ const ButtonComponent = ({ type, color, radius, children, btnText }) => {
         </div>
       );
     case "btn-icon":
-      return <div className={`btn_icon`}></div>;
+      return (
+        <div className={`btn_icon color_${color} btn_radius_${radius}`}>
+          {children}
+        </div>
+      );
     case "btn-float":
-      return <div className={`btn_float`}></div>;
+      return (
+        <div className={`btn_float color_${color} btn_radius_${radius}`}>
+          {children}
+        </div>
+      );
   }
 };
 
@@ -196,7 +210,7 @@ const ButtonPreview = () => {
               btnText="Delete"
               radius="medium"
             >
-              <AiFillDelete className="btn_icon_style" />
+              <AiOutlineDelete className="btn_icon_style" />
             </ButtonComponent>
             <p>Delete</p>
           </div>
@@ -217,15 +231,21 @@ const ButtonPreview = () => {
         <h2 className="button_heading_text">Only Icons</h2>
         <div className="button_box">
           <div className="button_wrapper">
-            {/* Component */}
+            <ButtonComponent type="btn-icon" color="secondary" radius="medium">
+              <BsFillCameraFill className="btn_icon_style" />
+            </ButtonComponent>
             <p>Capture</p>
           </div>
           <div className="button_wrapper">
-            {/* Component */}
+            <ButtonComponent type="btn-icon" color="danger" radius="medium">
+              <AiOutlineHeart className="btn_icon_style" />
+            </ButtonComponent>
             <p>Like</p>
           </div>
           <div className="button_wrapper">
-            {/* Component */}
+            <ButtonComponent type="btn-icon" color="primary" radius="small">
+              <BsBookmark className="btn_icon_style" />
+            </ButtonComponent>
             <p>Bookmark</p>
           </div>
         </div>
@@ -234,19 +254,27 @@ const ButtonPreview = () => {
         <h2 className="button_heading_text">Floating Action Buttons</h2>
         <div className="button_box">
           <div className="button_wrapper">
-            {/* Component */}
+            <ButtonComponent type="btn-float" color="primary" radius="small">
+              <IoAddOutline className="btn_icon_style" />
+            </ButtonComponent>
             <p>Add</p>
           </div>
           <div className="button_wrapper">
-            {/* Component */}
+            <ButtonComponent type="btn-float" color="secondary" radius="large">
+              <LiaEditSolid className="btn_icon_style" />
+            </ButtonComponent>
             <p>Edit</p>
           </div>
           <div className="button_wrapper">
-            {/* Component */}
+            <ButtonComponent type="btn-float" color="danger" radius="full">
+              <AiOutlineDelete className="btn_icon_style" />
+            </ButtonComponent>
             <p>Delete</p>
           </div>
           <div className="button_wrapper">
-            {/* Component */}
+            <ButtonComponent type="btn-float" color="warning" radius="medium">
+              <FiSearch className="btn_icon_style" />
+            </ButtonComponent>
             <p>Search</p>
           </div>
         </div>
@@ -255,7 +283,68 @@ const ButtonPreview = () => {
   );
 };
 
-const CodePreview = () => {};
+const CodePreview = () => {
+  const codeText = `  // Button Radius - radius: [small, medium, large, full]
+  <ButtonComponent type="btn-basic" color="default" radius="small" btnText="Button"/>
+  <ButtonComponent type="btn-basic" color="default" radius="medium" btnText="Button"/>
+  <ButtonComponent type="btn-basic" color="default" radius="large" btnText="Button"/>
+  <ButtonComponent type="btn-basic" color="default" radius="full" btnText="Button"/>
+  
+  // Basic Buttons - type: [btn-basic], color: [default, primary, secondary, success, warning, danger]
+  <ButtonComponent type="btn-basic" color="default" radius="medium" btnText="Default"/>
+  <ButtonComponent type="btn-basic" color="primary" radius="medium" btnText="Primary"/>
+  <ButtonComponent type="btn-basic" color="secondary" radius="medium" btnText="Secondary"/>
+  <ButtonComponent type="btn-basic" color="success" radius="medium" btnText="Success"/>
+  <ButtonComponent type="btn-basic" color="warning" radius="medium" btnText="Warning"/>
+  <ButtonComponent type="btn-basic" color="danger" radius="medium" btnText="Danger"/>
+
+  // Link Buttons - type: [btn-link]
+  <ButtonComponent type="btn-link" color="default" radius="small" btnText="Next Page"/>
+  // Link Button With Icon
+  <ButtonComponent type="btn-link" color="white" radius="full" btnText="Navigate">
+      <HiArrowSmallRight className="btn_icon_style" /> // Button Icon
+  </ButtonComponent>
+  <ButtonComponent type="btn-link" color="black" radius="full" btnText="Open link">
+      <BsBoxArrowUpRight className="btn_icon_style" /> // Button Icon
+  </ButtonComponent>
+
+  // Button with Icons - type: [btn-text-icon]
+  <ButtonComponent type="btn-text-icon" color="success" btnText="Upload" radius="full">
+      <BiSolidCloudUpload className="btn_icon_style" /> // Button Icon
+  </ButtonComponent>
+  <ButtonComponent type="btn-text-icon" color="danger" btnText="Delete" radius="medium">
+      <AiOutlineDelete className="btn_icon_style" /> // Button Icon
+  </ButtonComponent>
+  <ButtonComponent type="btn-text-icon" color="primary" btnText="Add" radius="small">
+      <IoAddOutline className="btn_icon_style" /> // Button Icon
+  </ButtonComponent>
+
+  // Only Icons - type: [btn-icon]
+  <ButtonComponent type="btn-icon" color="secondary" radius="medium">
+      <BsFillCameraFill className="btn_icon_style" /> // Button Icon
+  </ButtonComponent>
+  <ButtonComponent type="btn-icon" color="danger" radius="medium">
+      <AiOutlineHeart className="btn_icon_style" /> // Button Icon
+  </ButtonComponent>
+  <ButtonComponent type="btn-icon" color="primary" radius="small">
+      <BsBookmark className="btn_icon_style" /> // Button Icon
+  </ButtonComponent>
+
+  // Floating Action Buttons - type: [btn-float]
+  <ButtonComponent type="btn-float" color="primary" radius="small">
+      <IoAddOutline className="btn_icon_style" /> // Button Icon
+  </ButtonComponent>
+  <ButtonComponent type="btn-float" color="secondary" radius="large">
+      <LiaEditSolid className="btn_icon_style" /> // Button Icon
+  </ButtonComponent>
+  <ButtonComponent type="btn-float" color="danger" radius="full">
+      <AiOutlineDelete className="btn_icon_style" /> // Button Icon
+  </ButtonComponent>
+  <ButtonComponent type="btn-float" color="warning" radius="medium">
+      <FiSearch className="btn_icon_style" /> // Button Icon
+  </ButtonComponent>`;
+  return <CodeBlock codeText={codeText} />;
+};
 
 function Button() {
   const [showCode, setShowCode] = useState(false);
